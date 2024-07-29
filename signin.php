b40dcbb6a9aa612176bac26a6b97eed83301fa23
Jessica Carvalho <151664124+JessCarvalho29@users.noreply.github.com>
2024-07-29 12:23:58 -0400

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,5 +1,46 @@
 <?php
-include './inc/header.php';
+require_once('./inc/database.php');
+require './inc/header.php';
+
+$username = $_POST['username'];
+$password = hash('sha512', $_POST['password']);
+
+$validateInformations = true;
+
+if (empty($username)) {
+  echo '<p>Username is a required information to sign in</p>'; 
+  $validateInformations = false;
+}
+if (empty($password)) {
+  echo '<p>Password is a required information to sign in</p>';
+  $validateInformations = false;
+}
+
+if($validateInformations){
+  $DBreturn = $database->getLoginInfo($username, $password);
+
+  $result = mysqli_fetch_assoc($DBreturn);
+
+  if(isset($result['COUNT(username)']) && !empty($result['COUNT(username)'])){
+
+    session_start();
+    $_SESSION['timeout'] = time() + 30*60;
+
+    $_SESSION['user_id'] = $result['user_id'];
+    $firstName = $result['firstName'];
+    $lastName = $result['lastName'];
+
+    setcookie('firstName', $firstName, time() + 30*60, '/');
+    setcookie('lastName', $lastName, time() + 30*60, '/');
+
+    header('Location: view.php');
+
+  } else {
+    echo '<p>Invalid Login</p>';
+  }
+
+}
+
 ?>
 
 <main>
